fe39f9538f576e2bd23ee63385a85f52c3cf4e6c
Gerardine Alcala <alcala.gerardine@gmail.com>
2022-05-10 22:23:07 +0800

    added code for reviews method + to-do list

--- flask.py
+++ flask.py
@@ -7,10 +7,14 @@ app = Flask(__name__)
 def reviews():
     return render_template(url_for("index.html"))
 # Function for reviews.html
-@app.route("/")
+@app.route("/", methods=["POST"])
 def reviews():
-    return render_template(url_for("reviews.html"))
+    text = request.form["text"]
+    processedText = text.upper()
+    return processedText, render_template(url_for("reviews.html"))
 
 
 if __name__ == "__main__":
     app.run()
+
+# TO-DO: get flask to open index html on default; catch user input for number of tools to review and display csvData
